d427cf1e827aee87e364b2cdd2538ff74eab0e7d
Marco Canton <67104790+MarcoCanton1@users.noreply.github.com>
2022-05-19 15:55:38 -0300

    Repaso
    Finalización del repaso

---
+++ Assets/Activator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public GameObject cube;
+
+    void Start()
+    {
+        cube.SetActive(false);   
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cube.SetActive(!cube.activeInHierarchy);
+        }
+        
+    }
+}
